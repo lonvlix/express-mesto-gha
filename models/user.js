e48dbd889dca349e3343b56ee95ecbedcac0262e
lonvlix <llonvlix@yandex.ru>
2023-10-02 10:53:42 +0300

    new7

--- models/user.js
+++ models/user.js
@@ -22,10 +22,6 @@ const userSchema = new Schema(
       type: String,
       required: true,
       select: false,
-      validate: {
-        validator: ({ length }) => length >= 6,
-        message: 'Пароль должен состоять минимум из 6 символов',
-      },
     },
 
     name: {
